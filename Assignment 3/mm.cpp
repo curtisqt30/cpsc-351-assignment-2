bf97b37b8fac33cac7c70060495be68d10bdc1d2
curtisqt30 <143355730+curtisqt30@users.noreply.github.com>
2024-12-09 17:49:07 -0800

    Update mm.cpp

--- Assignment 3/mm.cpp
+++ Assignment 3/mm.cpp
@@ -9,9 +9,17 @@ struct Process {
 	int unique_id;
 	int arrival_time;
 	int lifetime;
-	int address_space;
 	std::vector<int> address_space;
 };
+
+// Blocks hold either a process or free spcae
+struct MemoryBlock {
+	int start;
+	int end;
+	int process_id;
+};
+
+std::vector<MemoryBlock> memory_map;
 
 // Function to read the input file.
 std::vector<Process> read_input(const std::string& file_path) {
@@ -23,13 +31,39 @@ std::vector<Process> read_input(const std::string& file_path) {
 	infile >> num_processes;
 	
 	for (int i = 0; i < num_processes; ++i) {
-		
+		Process p;
+		infile >> p.unique_id >> p.arrival_time >> p.lifetime;
+		int num_pages;
+		infile >> num_pages;
+		p.address_space.resize(num_pages);
+		for (int j = 0; j < num_pages; ++j) {
+			infile >> p.address_space[j];
+		}
+		processes.push_back(p);
 	}
 	
 	infile.close();
 	return processes;
 }
 
+// Function to simulate memory mapping
+void simulate_memory_map(std::vector<Process>& processes) {}
+
+
+// Function to print the memory map 
+void print_memory_map() {
+	std::cout << "Memory map:\n";
+	for (const auto& block : memory_map) {
+		if (block.process_id == -1) {
+			std::cout << "\t" << block.start << "-" << block.end << ": Free Frames(s)\n";
+		} else {
+			std::cout << "\t" << block.start << "-" << block.end << ": Process " << block.process_id << "\n";
+		}
+	}
+}
+
 int main() {
+	std::vector<Process> processes = read_input("in1.txt");
+	simulate_memory_map(processes);
 	return 0;
 }
